5dd4b4dffd4ff89c689b42f93f169c8dd790c5b3
mh-lamine <mlamins.ngom@gmail.com>
2025-03-18 14:31:59 +0100

    Add user data fetching and display in profile page

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,10 +1,44 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, ActivityIndicator, SafeAreaView, Pressable } from "react-native";
+import React, { useEffect, useState } from "react";
 
 export default function Page() {
-  return (
-    <View className="flex-1 bg-light justify-center items-center">
-      <Text>Mon compte</Text>
-    </View>
-  );
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const getUser = async () => {
+    try {
+      const response = await fetch("http://192.168.1.145:8080/api/user1");
+      const data = await response.json();
+      setUser(data);
+    } catch (error) {
+      console.error("Erreur lors de la récupération des données :", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  if (loading) {
+    return (
+      <View className="flex-1 bg-light justify-center items-center">
+        <ActivityIndicator size="large" color="#b361d4" />
+      </View>
+    );
+  }
+
+  if (user)
+    return (
+      <SafeAreaView className="flex-1 bg-light justify-center items-center">
+        <Text className="text-xl font-semibold">
+          {user.firstName} {user.lastName}
+        </Text>
+        <Text>{user.phoneNumber}</Text>
+        <Pressable className="bg-destructive px-4 py-2 rounded">
+          <Text className="text-white font-medium">Se déconnecter</Text>
+        </Pressable>
+      </SafeAreaView>
+    );
 }
--- app/_layout.jsx
+++ app/_layout.jsx
@@ -6,6 +6,7 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <StatusBar style="dark" />
     </Stack>
   );
 }
